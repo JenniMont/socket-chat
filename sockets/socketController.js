4f67b4d95026d39cfa940c7795a63e26f0736a67
Jenniffer Montoya <81304318+JenniMont@users.noreply.github.com>
2023-02-17 16:13:49 +0100

    first commit

---
+++ sockets/socketController.js
@@ -0,0 +1,9 @@
+// const { socket } = require('socket');
+
+const socketController = (socket) => {
+	// console.log('cliente conectado', socket.id);
+};
+
+module.exports = {
+	socketController,
+};
